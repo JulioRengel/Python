74537396c392f4cf85c32efe17c2c4de92193d78
Julio <rengel251@gmail.com>
2025-03-10 08:22:30 -0300

    Dia2Atualização

--- Dia1.py
+++ Dia1.py
@@ -1,4 +1,19 @@
-#Criando duas variáveis
-first_nome = 'Rafael' # String
-age = 25 # integer
-print('Olá, meu nome é', first_nome, 'e tenho', age, 'anos.') # Imprimindo as variáveis	
+
+# Variáveis
+# Variaveis primitivas: int, float, str, bool, none
+# Variaveis compostas: list, tuple, dict
+idade = 35 # int
+peso = 79.5 # float
+nome = 'João da Silva' # str
+ativo = True # bool
+resultado = None # none
+
+'''
+print(type(idade))
+print(type(peso))
+print(type(nome))
+print(type(ativo))
+print(type(resultado))
+'''
+# Meu nome é João da Silva, tenho 35 anos de idade.
+print('Meu nome é', nome, ', tenho', idade, 'anos de idade.')
---
+++ Dia2.py
@@ -0,0 +1,85 @@
+# Realizando Calculos com Numeros
+
+# num1 = 10
+# num2 = 5
+
+# resultado = num1 + num2
+# resultado = num1 // num2 # Divisão inteira
+# resultado = num1 % num2 # Resto da divisão
+# resultado = num1 ** num2 # Potenciação
+# print(resultado)
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+
+# print(num1)
+# print(num2)
+'''
+alunos_presentes = 23
+alunos_ausentes = '17'
+
+total_alunos = alunos_presentes + int(alunos_ausentes)
+
+print(total_alunos)
+'''
+
+'''
+# Operador de atribuição aumentada
+pro1 = 10
+
+# pro1 += 3
+# pro1 -= 3
+# pro1 *= 3
+# pro1 /= 3
+pro1 **= 3 # potenciação
+resultado = pro1
+
+print(resultado)
+'''
+
+
+'''
+# Funções basicas para numeros
+
+num1 = 3.5425
+
+print(round(num1, 3))
+
+print(num1)
+print(round(num1))
+
+print(pow(2, 3)) # 2 ** 3
+
+print(max(1, 4, 9, 2, 17, 3))
+print(min(1, 4, 9, 2, 17, 3))
+
+print(sum([1, 3, 7,]))
+'''
+
+'''
+# Orden dos operadores matematicos
+# 1 - () parenteses
+# 2 - ** potenciação
+# 3 - * multiplicação
+# 4 - / divisão
+# 5 - // divisão inteira
+# 6 - % resto da divisão / modulo
+# 7 - + adição
+# 8 - - subtração
+
+#resultado = 10 + 2 * 3
+resultado = (10 + 2) * 3
+print(resultado)
+
+resultado = 10 + 5 * 2 - 3
+print(resultado)
+'''
+
+# O modulo Math
+import math
+
+print(math.ceil(4.2)) # arredonda para cima
+print(math.floor(4.9)) # arredonda para baixo
+print(math.pow(2, 3)) # potenciação
+print(math.sqrt(16)) # raiz quadrada
