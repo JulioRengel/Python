47059795f4ccc4856a798b4b60a623b66f19adb0
Julio <rengel251@gmail.com>
2025-03-28 23:01:42 -0300

    Dia 8

---
+++ Aulas/Dia8.py
@@ -0,0 +1,46 @@
+# Operações matematicas em Python
+'''
+# Operadores de comparação
+
+operadores = 10 == 10
+print (operadores)
+
+operadores = 'banana' == 'Banana'
+print(operadores)
+
+operadores = 'banana' != 'Banana'
+print(operadores)
+
+operadores = 10 > 13
+print (operadores)
+
+operadores = 10 < 13
+print (operadores)
+
+operadores = 10 >= 10
+print (operadores)
+
+operadores = 10 <= 15
+print (operadores)
+
+#  == Equal
+#  != Not equal
+#   > Greater than
+#  < Less than 
+#  >= Greater than or equal to 
+#  <= Less than or equal to
+'''
+
+# Operadores de Atribuição 
+
+x = 10
+# x = x + 5
+# x += 5
+# x -= 5
+# x *= 5
+# x /= 5
+x %= 3
+print(x)
+
+
+
