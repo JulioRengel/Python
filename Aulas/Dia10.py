343de93bf7d6d1bc40937f03b5435b2af0d46383
Julio <rengel251@gmail.com>
2025-04-20 14:31:18 -0300

    Dia 10 final

--- Aulas/Dia10.py
+++ Aulas/Dia10.py
@@ -56,3 +56,62 @@ boasVindasRuben()
 boasVindasCarlos()
 boasVindasRicardo()
 '''
+'''
+#Argumetos Default e Non-default
+#Default = Aquele que voce define o valor no parametro. 
+#Non-default = Aquele qie voce não define o valor nó parametro. 
+
+def boasVindas(nome, quantidade=6):
+    print(f'Olá {nome}.')
+    print(f'Temos {str(quantidade)} laptops em estoque')
+
+boasVindas ('Ruben', 5)
+'''
+'''
+#Print ou Return em Funçoes
+#Funções 
+    #Realizan uma tarefa. 
+    #Calcula e retorna um valor. 
+
+def cliente1(nome):
+    print(f'Olá {nome}')
+
+def cliente2(nome):
+    return f'Olá {nome}'
+
+cliente1('Maria')
+
+print(cliente2('Jose'))
+'''
+'''
+#Varios argumetos xargs com numeros
+
+def soma(*numeros):
+    resultado = 0
+    for num in numeros:
+        resultado += num
+    return resultado
+
+x = soma(2,3,4,7)
+
+print (x)
+'''
+'''
+#Varios argumentos xargs nomenado parametros
+
+def agencia (**carro):
+    return carro
+
+
+print(agencia(Marca = 'Gol',Cor = 'Branca', Motor = 1.0))
+print(agencia(Marca = 'Gol',Cor = 'Branca', Motor = 1.0))
+print(agencia(Marca = 'Gol',Cor = 'Branca', Motor = 1.0, Placa = 'AML-4568'))
+'''
+
+#Importando um Modulo
+#¿ Qual é o numero fatorial de 4?
+    # 4 * 3 * 2 * 1 = 24
+
+import math
+
+print(math.factorial(4))
